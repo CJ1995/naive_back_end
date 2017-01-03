18cbfa8e360491e3985ba064e40ef4e8682a7ded
gdshen <gdshen95@gmail.com>
2017-01-03 22:49:22 +0800

    return table data

--- server/routes.py
+++ server/routes.py
@@ -1,6 +1,7 @@
-from server.views import index, post
+from server.views import index, post, table
 
 
 def setup_routes(app):
     app.router.add_get('/', index)
     app.router.add_post('/', post)
+    app.router.add_get('/table', table)
--- server/views.py
+++ server/views.py
@@ -6,6 +6,28 @@ async def index(request):
     a = {1: 2, 2: 3}
     return json_response(a)
 
+
+async def table(request):
+    data = [{
+        'date': '2016-05-03',
+        'name': 'Tom',
+        'address': 'No. 189, Grove St, Los Angeles',
+    }, {
+        'date': '2016-05-02',
+        'name': 'Tom2',
+        'address': 'No. 189, Grove St, Los Angeles',
+    }, {
+        'date': '2016-05-04',
+        'name': 'Tom3',
+        'address': 'No. 189, Grove St, Los Angeles',
+    }, {
+        'date': '2016-05-01',
+        'name': 'Tom4',
+        'address': 'No. 189, Grove St, Los Angeles',
+    }]
+    return json_response(data)
+
+
 async def post(request):
     data = await request.post()
     print(type(data))
